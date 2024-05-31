2cba0616185fb01b0fa8865f5d6c15a5b760eb06
Pavel <exactlypaulus@gmail.com>
2024-05-31 21:19:59 +0300

    feat: add icons for work section

--- src/components/sections/work/Work.tsx
+++ src/components/sections/work/Work.tsx
@@ -1,5 +1,10 @@
 import style from "./../../../styles/modules/work/work.module.scss";
 
+//Icons
+import modernIcon from "./../../../../public/modern.svg";
+import mobileIcon from "./../../../../public/mobile.svg";
+import devIcon from "./../../../../public/dev.svg";
+
 //components
 import SignItem from "./SignItem";
 import ProjectItem from "./ProjectItem";
@@ -7,9 +12,9 @@ import ProjectItem from "./ProjectItem";
 const Work = () => (
   <div className={style.work}>
     <ul className={style["signs-list"]}>
-      <SignItem label="Mobile-Friendly" icon="..." />
-      <SignItem label="Modern" icon="..." />
-      <SignItem label="Built with love" icon="..." />
+      <SignItem label="Mobile-Friendly" icon={mobileIcon.src} />
+      <SignItem label="Modern" icon={modernIcon.src} />
+      <SignItem label="Built with love" icon={devIcon.src} />
     </ul>
 
     <h3 className={style.heading}>Applications I've built</h3>
