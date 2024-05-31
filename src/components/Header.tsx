a2cd5b67b7eaf56e064f3a00e39efdc515a3266a
Pavel <exactlypaulus@gmail.com>
2024-05-31 16:14:15 +0300

    feat: add basic info for skills section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,10 @@ import LinkPoint from "./LinkPoint";
 const Header = () => (
   <header className={style.header}>
     <LinkPoint index={1} label="About" />
-    <LinkPoint index={2} label="Experience" />
-    <LinkPoint index={3} label="Work" />
-    <LinkPoint index={4} label="Contacts" />
+    <LinkPoint index={2} label="Skills" />
+    <LinkPoint index={3} label="Experience" />
+    <LinkPoint index={4} label="Work" />
+    <LinkPoint index={5} label="Contacts" />
   </header>
 );
 
