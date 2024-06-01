99a224dc3ab61d20f4afe678dfb0dff38167a241
Pavel <exactlypaulus@gmail.com>
2024-06-01 22:34:53 +0300

    fix: fix some styles

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ const Logo = () => (
   <div className={style.logo}>
     <span className={style.tag}>&lsaquo;</span>
 
-    <Link href="#" className={style["logo__logo-field"]}>
+    <Link href="#about" className={style["logo__logo-field"]}>
       <Image src={icon.src} width={35} height={35} alt="logo" />
       <span className={style["logo__label"]}>Exactly Paul</span>
     </Link>
