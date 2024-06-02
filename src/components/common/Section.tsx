d11c615e04af508ea03560106069602e3fa272c8
Pavel <exactlypaulus@gmail.com>
2024-06-02 17:39:14 +0300

    refactor: do dome refactoring

--- src/components/common/Section.tsx
+++ src/components/common/Section.tsx
@@ -1,18 +1,36 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
 import style from "./../../styles/modules/common/section.module.scss";
 import { IHeadingProps } from "./Heading";
 import Heading from "./Heading";
+
+//scripts
+import setBgLabelSlipEffect from "@/scripts/setBgLabelSlipEffect";
 
 interface ISectionProps extends IHeadingProps {
   content: React.ReactNode;
   id: string;
 }
 
-const Section = (props: ISectionProps) => (
-  <section className={style.section} id={props.id}>
-    <Heading index={props.index} label={props.label} />
-    <span className={style["background-label"]}>{props.label}</span>
-    <>{props.content}</>
-  </section>
-);
+const Section = (props: ISectionProps) => {
+  const bgLabelRef = useRef(null);
+
+  useEffect(() => {
+    const bgLabelNode: HTMLElement = bgLabelRef.current!;
+    setBgLabelSlipEffect(bgLabelNode);
+  }, []);
+
+  return (
+    <section className={style.section} id={props.id}>
+      <Heading index={props.index} label={props.label} />
+      <span className={style["background-label"]} ref={bgLabelRef}>
+        {props.label}
+      </span>
+      <>{props.content}</>
+    </section>
+  );
+};
 
 export default Section;
---
+++ src/scripts/setBgLabelSlipEffect.ts
@@ -0,0 +1,21 @@
+//this animation script is only for .background-label in Section component
+
+import style from "../styles/modules/common/section.module.scss";
+
+const setBgLabelSlipEffect = (label: HTMLElement) => {
+  label.classList.add(style["hide-bg-label"]);
+
+  const observerCallback = (entries: IntersectionObserverEntry[]) => {
+    const [entry] = entries;
+
+    if (entry.isIntersecting) {
+      entry.target.classList.remove(style["hide-bg-label"]);
+      observer.unobserve(label);
+    }
+  };
+
+  const observer = new IntersectionObserver(observerCallback, { threshold: 1 });
+  observer.observe(label);
+};
+
+export default setBgLabelSlipEffect;
