201a1df26f79c3b49718bb471230a953b1ca9c3b
Pavel <exactlypaulus@gmail.com>
2024-06-05 01:35:52 +0300

    fix: remove extra code

--- src/components/sections/work/CommingProjectCardItem.tsx
+++ src/components/sections/work/CommingProjectCardItem.tsx
@@ -2,26 +2,14 @@
 
 import style from "./../../../styles/modules/work/commingProjectCardItem.module.scss";
 
-import { useRef, useEffect } from "react";
-
 import Image from "next/image";
 
 //IMG
 import codingImg from "./../../../../public/coding.jpg";
 
-//scripts
-import setFallEffect from "@/scripts/startAnimations/setFallEffect";
-
 const CommingProjectCardItem = () => {
-  const commingProjectRef = useRef(null);
-
-  useEffect(() => {
-    const commingProjectNode: HTMLElement = commingProjectRef.current!;
-    setFallEffect(commingProjectNode);
-  }, []);
-
   return (
-    <div className={`${style["comming-project"]} fallInit`} ref={commingProjectRef}>
+    <div className={`${style["comming-project"]} fallInit`}>
       <Image src={codingImg.src} fill={true} alt="coding" sizes="100%" />
       <div className={style.veil}></div>
       <p className={style.notice}>Comming soon</p>
