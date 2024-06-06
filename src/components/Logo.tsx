be63b8604a4a16ac43f20da376ee38b0ee343005
Pavel <exactlypaulus@gmail.com>
2024-06-06 21:06:55 +0300

    fix: fix some bugs

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,11 +5,19 @@ import style from "./../styles/modules/logo.module.scss";
 
 import icon from "../../public/logo.svg";
 
+import displayMobileNav from "@/scripts/displayMobileNav";
+
 const Logo = () => (
   <div className={style.logo}>
     <span className={style.tag}>&lsaquo;</span>
 
-    <Link href="#about" className={style["logo__logo-field"]}>
+    <Link
+      href="#about"
+      className={style["logo__logo-field"]}
+      onClick={() => {
+        displayMobileNav(true);
+      }}
+    >
       <Image src={icon.src} width={35} height={35} alt="logo" />
       <span className={style["logo__label"]}>Exactly Paul</span>
     </Link>
