1becebcb9f817f7f00010f410b2e1c98a1d2cb30
Pavel <exactlypaulus@gmail.com>
2024-06-06 14:06:51 +0300

    fix: fix item card

--- src/components/sections/work/ProjectCardItem.tsx
+++ src/components/sections/work/ProjectCardItem.tsx
@@ -47,27 +47,29 @@ const ProjectCardItem = (props: IProjectCardPortalProps) => {
         className={`${style["project-card-portal"]} openPortal`}
         onClick={(e) => e.stopPropagation()}
       >
-        <Button extraClass={style["close-btn"]} clickAction={closeProjectCardItem}>
-          Close
-        </Button>
+        <div className={style.info}>
+          <Button extraClass={style["close-btn"]} clickAction={closeProjectCardItem}>
+            Close
+          </Button>
 
-        <hr className={style.portition} />
+          <hr className={style.portition} />
 
-        <h3>{title}</h3>
+          <h3>{title}</h3>
 
-        <div className={style["img-frame"]}>
-          <Image src={`/projects/${img}`} alt="project preview image" fill={true} sizes="100%" />
+          <div className={style["img-frame"]}>
+            <Image src={`/projects/${img}`} alt="project preview image" fill={true} sizes="100%" />
+          </div>
+
+          <h3>About</h3>
+          <p>{desc.common}</p>
+
+          <h3>Technologies</h3>
+
+          <TechList techList={desc.technical} />
+
+          <SourceLink link={deploy} type="Website" />
+          <SourceLink link={github} type="Github" />
         </div>
-
-        <h3>About</h3>
-        <p>{desc.common}</p>
-
-        <h3>Technologies</h3>
-
-        <TechList techList={desc.technical} />
-
-        <SourceLink link={deploy} type="Website" />
-        <SourceLink link={github} type="Github" />
 
         <Button extraClass={style["deploy-link"]}>
           Open the project
