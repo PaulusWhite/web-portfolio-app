04439c6d56e252da050a7836f151a6c9e99ebaf1
Pavel <exactlypaulus@gmail.com>
2024-06-08 00:21:20 +0300

    refactor:refactor api route

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -4,8 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 import { IPostRequestBody, IResponseData } from "@/interfaces/IPostMessageByEmail";
 
 const URL: string = "https://api.web3forms.com/submit";
+
 const SUCCESS_RESPONSE_MESSAGE: string =
   "Your message was sent successfully! I will answer you as soon as possible!";
+
 const ERROR_RESPONSE_MESSAGE: string =
   "An error happened on the server. Please, contact me via LinkedIn or write me directly from you email!";
 
@@ -26,15 +28,12 @@ export async function POST(request: NextRequest): Promise<NextResponse<IResponse
 
   const result = await response.json();
 
-  const responseData: IResponseData = {
+  let responseData: IResponseData = {
     isError: false,
     message: SUCCESS_RESPONSE_MESSAGE,
   };
 
-  if (!result.success) {
-    responseData.isError = true;
-    responseData.message = ERROR_RESPONSE_MESSAGE;
-  }
+  if (!result.success) responseData = { isError: true, message: ERROR_RESPONSE_MESSAGE };
 
   return NextResponse.json(responseData);
 }
