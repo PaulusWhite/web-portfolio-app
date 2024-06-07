e48880b390d6971090972b7b168e9b7fc247538e
Pavel <exactlypaulus@gmail.com>
2024-06-08 00:59:49 +0300

    refactor: remove canceled script

--- src/scripts/flashlightEffect.ts
+++
@@ -1,7 +0,0 @@
-const flashlightEffect = (flashlight: HTMLDivElement) => {
-  window.addEventListener("mousemove", (e: MouseEvent) => {
-    flashlight.style.cssText = `left: ${e.pageX}px; top: ${e.pageY}px`;
-  });
-};
-
-export default flashlightEffect;
